58f434afa1b289d84a7207207c1ae2dc90a1409d
meaella <menaelena@yandex.com>
2019-01-30 10:41:47 -0700

    I learned bout the typeof operator checks the value to its right and returns, or passes back, a string of the data type.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/typeof-operator.js
@@ -0,0 +1,10 @@
+
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+
+/*
+I learned bout the typeof operator checks the value to its right and returns,
+ or passes back, a string of the data type.
+*/
