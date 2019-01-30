6dd86f5e637cf9f1b85081ad3933d41694df718f
meaella <menaelena@yandex.com>
2019-01-30 10:37:58 -0700

    I leaned that in JavaScript, we can insert, or interpolate, variables into strings using template literals.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Interpolation.js
@@ -0,0 +1,8 @@
+
+let myName = 'Elena';
+let myCity = 'Littleton';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
+/*
+ I leaned that in JavaScript, we can insert, or interpolate, variables into strings using template literals.
+*/
