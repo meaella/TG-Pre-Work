56a1679083b1dbd1685960798f1ac37750db3ae5
meaella <menaelena@yandex.com>
2019-01-30 10:16:08 -0700

    This ":" char in the filename "Create-a-Variable:letr" not Allowed on Windows. I renamed the file to the "Create-a-Variable_let"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable_let.js
@@ -0,0 +1,9 @@
+
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
+
+/*
+ I learned that The let keyword signals that the variable can be reassigned a different value and
+ we can declare a variable without assigning the variable a value
+*/
