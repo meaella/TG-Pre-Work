b466232b685be9211e15507f0b63f269e1278bb9
meaella <menaelena@yandex.com>
2019-01-30 10:30:52 -0700

    I learned that two operators increment operator (++) and decrement operator (--).  The increment operator will increase the value of the variable by 1. The decrement operator will decrease the value of the variable by 1.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/The-Increment-and-Decrement-Operator.js
@@ -0,0 +1,11 @@
+
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
+
+/*
+ I learned that two operators increment operator (++) and decrement operator (--).
+ The increment operator will increase the value of the variable by 1.
+ The decrement operator will decrease the value of the variable by 1.
+*/
