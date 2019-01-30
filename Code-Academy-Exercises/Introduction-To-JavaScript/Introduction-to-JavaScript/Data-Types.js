e14040e66ca0b13603445e402a7be309f24b57db
meaella <menaelena@yandex.com>
2019-01-30 09:35:08 -0700

    I learned that in JavaScript are seven data types: Number, String, Boolean, Null, Undefined, Symbol, Object

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Data-Types.js
@@ -0,0 +1,6 @@
+
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+
+//I learned that in JavaScript are seven data types: Number, String, Boolean, Null, Undefined, Symbol, Object.
