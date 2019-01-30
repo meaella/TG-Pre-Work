4f69828bd9c4a6de385bdb1c6b59982e8e548b6e
meaella <menaelena@yandex.com>
2019-01-30 09:59:45 -0700

    I leaned that a variable is a container for a value

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Variables.js
@@ -0,0 +1,10 @@
+
+
+/*
+I leaned that a variable is a container for a value,
+variables also provide a way of labeling data with a descriptive name.
+There are only a few things I can do with variables:
+1.Create a variable with a descriptive name.
+2.Store or update information stored in a variable.
+3.Reference or “get” information stored in a variable.
+*/
