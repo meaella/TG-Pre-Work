0fb2ee3bfdf93c62234aac146448096c8d1137e8
meaella <menaelena@yandex.com>
2019-01-30 10:07:17 -0700

    This ":" char in the filename "Create-a-Variable:var" not Allowed on Windows. I renamed the file to the "Create-a-Variable_var"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable_var.js
@@ -0,0 +1,11 @@
+
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+
+/*
+ I leaned that t`here were a lot of changes introduced in the ES6 version of JavaScript in 2015.
+ One of the biggest changes was two new keywords, let and const, to create, or declare, variables.
+ Prior to the ES6, programmers could only use the var keyword to declare variables.
+*/
