9f0489c978393268fb5096018ccc3691ff620533
meaella <menaelena@yandex.com>
2019-01-30 10:26:32 -0700

    I learned that we can performing the mathematical operation of the first operator +(-)  using the number to the right, then reassigning variable to the computed value.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Mathematical-Assignment-Operators.js
@@ -0,0 +1,10 @@
+
+levelUp += 5;
+powerLevel -=100;
+multiplyMe *= 11;
+quarterMe /= 4
+
+/*
+ I learned that we can performing the mathematical operation of the first operator +(-)
+ using the number to the right, then reassigning variable to the computed value.
+*/
