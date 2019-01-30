1d17ce625ccd098f0d4ea0e4633645e1e57c19ab
meaella <menaelena@yandex.com>
2019-01-30 10:34:02 -0700

    I leaned that the + operator can be used to combine two string values even  if those values are being stored in variables.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/String-Concatenation-with-Variables.js
@@ -0,0 +1,8 @@
+
+let favoriteAnimal ='cat';
+console.log('My favorite animal:'+ favoriteAnimal);
+
+/*
+ I leaned that the + operator can be used to combine two string values even
+ if those values are being stored in variables.
+*/
