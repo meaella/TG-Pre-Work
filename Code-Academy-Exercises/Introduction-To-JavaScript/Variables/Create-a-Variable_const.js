040504ebea2d286dd2bb71d39fb943be95f91957
meaella <menaelena@yandex.com>
2019-01-30 10:20:30 -0700

    This ":" char in the filename "Create-a-Variable:const" not Allowed on Windows. I renamed the file to the "Create-a-Variable_const"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Variables/Create-a-Variable_const.js
@@ -0,0 +1,9 @@
+
+const entree = 'Enchiladas';
+console.log(entree);
+entree = 'Tacos';
+
+/*
+ I learned that The const keyword was also introduced in ES6, and is short for the word constant.
+ However, a const variable cannot be reassigned because it is constant.
+*/
