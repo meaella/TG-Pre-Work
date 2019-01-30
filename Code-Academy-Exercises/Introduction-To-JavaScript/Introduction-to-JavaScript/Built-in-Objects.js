be6fc58e664b755f09f47952acc50617701d0dd6
meaella <menaelena@yandex.com>
2019-01-30 09:55:57 -0700

    I learne about 4 methods: Math.random() generates a random number between 0 and 1, Math.floor() rounds the number down to the nearest whole number, Number.isInteger() determine whether the passed value is an integer.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Introduction-to-JavaScript/Built-in-Objects.js
@@ -0,0 +1,10 @@
+
+console.log(Math.floor(Math.random()*100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+/* I learne about 4 methods: 
+Math.random() generates a random number between 0 and 1,
+Math.floor() rounds the number down to the nearest whole number,
+Number.isInteger() determine whether the passed value is an integer.
+*/
