7da4231479d4292b57d070dbae3b37900aae31ce
meaella <menaelena@yandex.com>
2019-01-31 11:52:37 -0700

    I learned about if-else decisions that can be modeled in code by creating conditional statements. A conditional statement checks specific condition(s) and performs a task based on the condition(s)

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/What-are-Conditional-Statements.js
@@ -0,0 +1,4 @@
+
+
+/* I learned about if-else decisions that can be modeled in code by creating conditional statements.
+ A conditional statement checks specific condition(s) and performs a task based on the condition(s) */
