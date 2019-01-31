c48fc5baca21934a56fa64bf437741938432b972
meaella <menaelena@yandex.com>
2019-01-31 12:57:15 -0700

    I learned that functions can take inputs and use the inputs to perform a task. When declaring a function, we can specify its parameters

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Parameters-and-Arguments.js
@@ -0,0 +1,9 @@
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+
+sayThanks('Cole');
+
+/* I learned that functions can take inputs and use the inputs to perform a task.
+ When declaring a function, we can specify its parameters */
