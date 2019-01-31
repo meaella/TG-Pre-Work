998e75a6ceab23786e021dc2e653fe11a0fc1603
meaella <menaelena@yandex.com>
2019-01-31 12:03:28 -0700

    I learned about comparison operators that compare the value on the left with the value on the right

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Comparison-Operators.js
@@ -0,0 +1,10 @@
+
+let hungerLevel = 7;
+if (hungerLevel>7){
+  console.log('Time to eat!');
+}
+else {
+  console.log('We can eat later!');
+}
+
+// I learned about comparison operators that compare the value on the left with the value on the right
