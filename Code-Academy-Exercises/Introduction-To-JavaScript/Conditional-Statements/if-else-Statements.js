21ee54485a1e0cd523f8569d0abf7ea39aa3760a
meaella <menaelena@yandex.com>
2019-01-31 12:00:37 -0700

    I learned that if we wanted to add some default behavior to the if statement, we can add an else statement to run a block of code when the condition evaluates to false

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/if-else-Statements.js
@@ -0,0 +1,10 @@
+
+if(sale) {
+  console.log('Time to buy!');
+}
+else {
+  console.log('Time to wait for a sale.');
+}
+
+/* I learned that if we wanted to add some default behavior to the if statement,
+ we can add an else statement to run a block of code when the condition evaluates to false */
