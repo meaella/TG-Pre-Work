d7bafa2e96b2169addcf5cfc4bb14a4da4ecfe4e
meaella <menaelena@yandex.com>
2019-01-31 12:15:31 -0700

    I learned about that variables not necessarily equals a specific value, sometimes need only check to see if the variable has been assigned a value

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy.js
@@ -0,0 +1,18 @@
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+/* I learned about that variables not necessarily equals a specific value, sometimes
+need only check to see if the variable has been assigned a value */
