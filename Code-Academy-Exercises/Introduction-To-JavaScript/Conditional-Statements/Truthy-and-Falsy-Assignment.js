f2841316748e3187ffb8bea8110a8e1e0f6ccb41
meaella <menaelena@yandex.com>
2019-01-31 12:22:02 -0700

    I learned about short-circuit evaluation to assign a value Truthy or Falsy

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Conditional-Statements/Truthy-and-Falsy-Assignment.js
@@ -0,0 +1,8 @@
+
+let tool = 'marker';
+
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+// I learned about short-circuit evaluation to assign a value Truthy or Falsy  
