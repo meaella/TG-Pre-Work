52765188d2ec41ec2577705a46029ac0e054ad87
meaella <menaelena@yandex.com>
2019-01-31 12:52:07 -0700

    I learned about that a function declaration consists of the function keyword, the name of the function followed by parentheses. A function body, required to perform a specific task, enclosed in the function’s curly brackets, { }

--- Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Declaration.js
@@ -2,7 +2,11 @@
 function getReminder(){
  console.log('Water the plants.');
 }
+function greetInSpanish(){
+ console.log('Buenas Tardes.');
+}
 
-/* I learned about that a function declaration consists of the function keyword,
+
+/* I learned about that a function declaration consists of the function keyword, 
 the name of the function followed by parentheses. A function body, required to perform a specific task,
- enclosed in the function’s curly brackets, { } */
+enclosed in the function’s curly brackets, { } */
