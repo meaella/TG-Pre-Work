6f386bb9d63e9d10cbb7cb88a6c194f328552c93
meaella <menaelena@yandex.com>
2019-01-31 13:03:28 -0700

    I learned that to pass back information from the function call, we use a return statement

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Return.js
@@ -0,0 +1,8 @@
+
+function monitorCount(rows, columns){
+  return rows*columns;
+}
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors);
+
+// I learned that to pass back information from the function call, we use a return statement
