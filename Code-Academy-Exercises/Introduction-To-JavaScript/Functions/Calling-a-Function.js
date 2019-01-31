1f08b7f399b4cd7064beb298c72506dc207edb94
meaella <menaelena@yandex.com>
2019-01-31 12:54:53 -0700

    I learned that the function call executes the function body, or all of the statements between the curly braces in the function declaration

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Calling-a-Function.js
@@ -0,0 +1,11 @@
+
+
+function sayThanks(){
+  console.log('Thank you for your purchase! We appreciate your business.');
+}
+
+sayThanks();
+sayThanks();
+sayThanks();
+
+// I learned that the function call executes the function body, or all of the statements between the curly braces in the function declaration.  
