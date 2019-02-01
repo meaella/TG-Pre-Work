02e58e24ae573625be6b699f318a7f04a64f8a78
meaella <menaelena@yandex.com>
2019-02-01 12:37:34 -0700

    I learned about an instance - an object that contains the property names and methods of a class, but with unique property values

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Instance.js
@@ -0,0 +1,12 @@
+
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+
+// I learned about an instance - an object that contains the property names and methods of a class, but with unique property values.
