a1e1f5a25fd53cfc2fa700ba726e31f807306927
meaella <menaelena@yandex.com>
2019-02-01 13:58:59 -0700

    I leaned about that e need npm install command to install new Node packages locally.  The install command creates a folder called node_modules and copies the package files to it. The install command also installs all of the dependencies for the given package

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Install-Node-Packages.js
@@ -0,0 +1,4 @@
+
+
+/* I leaned about that e need npm install command to install new Node packages locally.
+ The install command creates a folder called node_modules and copies the package files to it. The install command also installs all of the dependencies for the given package */
