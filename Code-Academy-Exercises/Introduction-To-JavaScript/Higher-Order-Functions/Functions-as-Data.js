f05012043c9f8a0f6fb23605c74b54658b2cd099
meaella <menaelena@yandex.com>
2019-02-01 15:35:20 -0700

    I learned tha JavaScript functions behave like any other data type in the language; we can assign functions to variables, and we can reassign them to new variable

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Functions-as-Data.js
@@ -0,0 +1,7 @@
+
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+is2p2();
+console.log(is2p2.name);
+
+/* I learned tha JavaScript functions behave like any other data type in the language;
+we can assign functions to variables, and we can reassign them to new variables */
