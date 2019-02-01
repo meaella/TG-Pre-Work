a9f4f5cf858a74004f68d3a3093ff58c74f524a5
meaella <menaelena@yandex.com>
2019-01-31 17:57:32 -0700

    I learned that a function expression is often stored in a variable in order to refer to it

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Expressions.js
@@ -0,0 +1,8 @@
+
+const plantNeedsWater = function(day, plantNeedsWater){
+  if (day === 'Wednesday'){
+    return true;
+  }
+};
+
+// I learned that a function expression is often stored in a variable in order to refer to it
