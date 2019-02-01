3cb7ccef0ed37b242839332185e03ad19975fb74
meaella <menaelena@yandex.com>
2019-02-01 11:40:58 -0700

    I learned that an objects can be assigned to variables just like any JavaScript type

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Creating-Object-Liberals.js
@@ -0,0 +1,8 @@
+
+// Write your fasterShip object literal below
+let fasterShip = {
+  'Fuel Type': 'Turbo Fuel',
+  color: 'silver'
+};
+
+// I learned that an objects can be assigned to variables just like any JavaScript type
