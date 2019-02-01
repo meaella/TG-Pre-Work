18e7a2aaadc4dea7908534babe047bb91d2bf7ce
meaella <menaelena@yandex.com>
2019-02-01 16:15:06 -0700

    I learne that in JavaScript developers follow naming conventions that signal to other developers  how to interact with a property.  One common convention is to place an underscore _ before the name of a property to mean that the property  should not be altered

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Privacy.js
@@ -0,0 +1,8 @@
+
+robot._energyLevel = 'high';
+robot.recharge()
+
+/* I learne that in JavaScript developers follow naming conventions that signal to other developers
+ how to interact with a property.
+ One common convention is to place an underscore _ before the name of a property to mean that the property
+ should not be altered */.
