c1543d56d2694845f82f22a99ee6af0e161566ab
meaella <menaelena@yandex.com>
2019-02-01 12:34:36 -0700

    I learned taht JavaScript calls the constructor() method every time it creates a new instance of a class

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Constructor.js
@@ -0,0 +1,9 @@
+
+class Surgeon {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+}
+
+// I learned taht JavaScript calls the constructor() method every time it creates a new instance of a class
