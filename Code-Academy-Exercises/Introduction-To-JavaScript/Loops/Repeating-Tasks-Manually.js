f2766cca7e2ebc3daf28892ca58eef3e01288eca
meaella <menaelena@yandex.com>
2019-02-01 09:43:08 -0700

    I learned that if a repeated task required you to type out the same code every single time - we need the loop.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,8 @@
+
+let vacationSpots = ['Moscow','London','Paris'];
+
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+ // I learned that if a repeated task required you to type out the same code every single time - we need the loop.
