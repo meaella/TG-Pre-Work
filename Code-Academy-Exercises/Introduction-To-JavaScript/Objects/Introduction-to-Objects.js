3c7571c1757463f890dcdea843d9d4c1e9f510f0
meaella <menaelena@yandex.com>
2019-02-01 11:38:50 -0700

    I learned that many components of the language are actually objects under the hood, and even the parts that aren't like strings or numbers can still act like objects in some instances

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Introduction-to-Objects.js
@@ -0,0 +1,4 @@
+
+
+/* I learned that many components of the language are actually objects under the hood,
+ and even the parts that aren't like strings or numbers can still act like objects in some instances */
