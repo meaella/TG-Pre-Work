6a91d1163dd0c9efb799d4394573206bcc2400c5
meaella <menaelena@yandex.com>
2019-01-31 20:26:45 -0700

    I learned about push() methods allows us to add items to the end of an array

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-push-method.js
@@ -0,0 +1,7 @@
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+chores.push('do shoping','do dinner')
+console.log(chores);
+
+// I learned about push() methods allows us to add items to the end of an array
