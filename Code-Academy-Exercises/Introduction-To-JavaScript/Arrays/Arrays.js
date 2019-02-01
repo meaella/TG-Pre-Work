3a6a7b71f6158341af1a86168314fb5122ac85cc
meaella <menaelena@yandex.com>
2019-01-31 20:05:51 -0700

    I learned about arrays and that they can store any data types. Arrays are ordered, meaning each item has a numbered position

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays.js
@@ -0,0 +1,6 @@
+
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+
+console.log(newYearsResolutions);
+
+// I learned about arrays and that they can store any data types. Arrays are ordered, meaning each item has a numbered position.
