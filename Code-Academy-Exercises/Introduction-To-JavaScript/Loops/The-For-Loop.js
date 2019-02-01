b11557f37f16c9eccf05a937edffe3ee458a87a1
meaella <menaelena@yandex.com>
2019-02-01 09:50:41 -0700

    I learned about "For" loop includes an iterator variable that usually appears in all three expressions: 1. The iterator variable is initialized. 2. The iterator variable checked against the stopping condition. 3. The iterator variable assigned a new value on each loop iteration.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/The-For-Loop.js
@@ -0,0 +1,9 @@
+
+for (let counter = 5; counter < 11; counter++) {
+  console.log(counter);
+}
+
+/* I learned about "For" loop includes an iterator variable that usually appears in all three expressions:
+1. The iterator variable is initialized.
+2. The iterator variable checked against the stopping condition.
+3. The iterator variable assigned a new value on each loop iteration. */
