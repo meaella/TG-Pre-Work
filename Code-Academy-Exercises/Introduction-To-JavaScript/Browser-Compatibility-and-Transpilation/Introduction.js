eac5307d9957ba65b4e7b7ebe70e4fe6c7d6367f
meaella <menaelena@yandex.com>
2019-02-01 13:39:38 -0700

    I learned about caniuse.com a website that provides data on web browser compatibility for HTML, CSS, and JavaScript features. Babel a Javascript library that you can use to convert new, unsupported JavaScript (ES6), into an older version (ES5) that is recognized by most modern browsers

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Introduction.js
@@ -0,0 +1,4 @@
+
+
+/* I learned about caniuse.com a website that provides data on web browser compatibility for HTML, CSS, and JavaScript features.
+Babel a Javascript library that you can use to convert new, unsupported JavaScript (ES6), into an older version (ES5) that is recognized by most modern browsers */
