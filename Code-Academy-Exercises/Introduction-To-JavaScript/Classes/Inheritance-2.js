bb91824b7849028af4d9a3cb72abbde66a1dc3fd
meaella <menaelena@yandex.com>
2019-02-01 12:56:41 -0700

    I learned how class inherits information from the parent class.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-2.js
@@ -0,0 +1,18 @@
+
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  get name() {
+    return this._name;
+  }
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+}
+
+// I learned how class inherits information from the parent class.
