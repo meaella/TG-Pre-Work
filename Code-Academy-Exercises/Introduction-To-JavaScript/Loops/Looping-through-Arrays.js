1067959b607e24c6e2b8d2476e16f974b786022d
meaella <menaelena@yandex.com>
2019-02-01 10:00:58 -0700

    I learned we should use the array's ".length" property in a condition

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Looping-through-Arrays.js
@@ -0,0 +1,9 @@
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i=0; i<vacationSpots.length; i++) {
+	 console.log('I would love to visit ' + vacationSpots[i]);
+}
+
+// I learned we should use the array's ".length" property in a condition. 
