7dbc2adfdb960a430856a7737239bedcdf239004
meaella <menaelena@yandex.com>
2019-02-01 13:46:09 -0700

    I learned about how many % of usage the ES5 and "Template Literals"

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,7 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 95.38;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.67;
+
+// I learned about how many % of usage the ES5 and "Template Literals"
