89613368fe603f7a1a8290c5edce35838264af48
meaella <menaelena@yandex.com>
2019-02-01 13:26:42 -0700

    I learned how to create an object that inherits properties from a parent class let's turn our attention to methods

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Inheritance-4.js
@@ -0,0 +1,6 @@
+
+
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+
+// I learned how to create an object that inherits properties from a parent class let's turn our attention to methods
