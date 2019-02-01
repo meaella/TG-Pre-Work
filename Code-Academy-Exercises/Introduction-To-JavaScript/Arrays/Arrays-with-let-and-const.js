88ec8aa54d20d33176674edb8e28538060dc0451
meaella <menaelena@yandex.com>
2019-01-31 20:21:29 -0700

    I leraned taht I can declare variables with both the let and const keywords. Variables declared with let can be reassigned. elements in an array declared with const remain mutable

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Arrays-with-let-and-const.js
@@ -0,0 +1,14 @@
+
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments[0]);
+condiments = ['Mayo'];
+console.log(condiments);
+
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+/* I leraned taht I can declare variables with both the let and const keywords. Variables declared with let can be reassigned. elements in an array declared with const remain mutable */
