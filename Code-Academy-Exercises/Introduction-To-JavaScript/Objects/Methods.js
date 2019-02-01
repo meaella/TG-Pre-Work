f90dd0f86c746e8806948be2244865a991feef1c
meaella <menaelena@yandex.com>
2019-02-01 11:57:49 -0700

    I learned about methods is what an object does

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Methods.js
@@ -0,0 +1,13 @@
+
+let alienShip = {
+  retreat: function(){
+    console.log(retreatMessage);
+  },
+  takeOff: function (){
+    console.log('Spim... Borp... Glix... Blastoff!');
+  }
+}
+alienShip.retreat();
+alienShip.takeOff();
+
+// I learned about methods is what an object does
