0933e2b4df62806b27d87ced1b3b632710e9c6dd
meaella <menaelena@yandex.com>
2019-02-01 12:49:51 -0700

    I learned about using methods to access and manipulate data from instances

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Classes/Method-Calls.js
@@ -0,0 +1,6 @@
+
+console.log(surgeonCurry.name);
+surgeonCurry.takeVacationDays(3);
+console.log(surgeonCurry.remainingVacationDays);
+
+// I learned about using methods to access and manipulate data from instances.
