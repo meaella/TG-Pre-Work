4d9f2ad6665164ae5742c707aaf2068bba585243
meaella <menaelena@yandex.com>
2019-01-31 20:13:12 -0700

    I learned that to have access to individual characters in a string must use bracket notation and the index. Arrays in JavaScript are zero-indexed, meaning

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/Accessing-Elements.js
@@ -0,0 +1,10 @@
+
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+
+let listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
+
+/* I learned that to have access to individual characters in a string must use bracket notation and the index.
+Arrays in JavaScript are zero-indexed, meaning the positions start counting from 0 rather than 1 */
