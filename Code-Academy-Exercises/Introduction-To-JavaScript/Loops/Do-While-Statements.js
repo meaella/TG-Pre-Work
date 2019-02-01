ef8ae9b1f0375187987bea0f131ce33b0c998352
meaella <menaelena@yandex.com>
2019-02-01 10:28:32 -0700

    I learned about "do...while" that says to do a task once and then keep doing it until a specified condition is no longer met

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Do-While-Statements.js
@@ -0,0 +1,8 @@
+
+let cupsOfSugarNeeded = 12;
+let cupsAdded = 8;
+ do {
+   cupsAdded +=1;
+ } while (cupsAdded < cupsOfSugarNeeded);
+
+// I learned about "do...while" that says to do a task once and then keep doing it until a specified condition is no longer met.
