96047f96a3c9e2e370641e1a0a1c4e9aebc07279
meaella <menaelena@yandex.com>
2019-01-31 20:24:25 -0700

    I learned aboutan array's propertie -length and it returns the number of items in the array

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/The-length-property.js
@@ -0,0 +1,5 @@
+
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
+
+// I learned aboutan array's propertie -length and it returns the number of items in the array
