ad930dc2b88c527ccc1d54333092420f9c5de929
meaella <menaelena@yandex.com>
2019-02-01 11:52:08 -0700

    I learned that we can  either dot notation, ., or bracket notation, [], and the assignment operator, =  to add new key-value pairs to an object or change an existing property

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Property-Assignment.js
@@ -0,0 +1,7 @@
+
+spaceship.color = 'glorious gold';
+spaceship.numEngines = 9;
+delete spaceship['Secret Mission'];
+
+/* I learned that we can  either dot notation, ., or bracket notation, [], and the assignment operator, = 
+to add new key-value pairs to an object or change an existing property */
