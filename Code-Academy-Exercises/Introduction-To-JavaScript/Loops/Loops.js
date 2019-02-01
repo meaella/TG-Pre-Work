8eb5a80db76474aefec6e48f0857d1f2cfaf562c
meaella <menaelena@yandex.com>
2019-02-01 09:37:55 -0700

    I learned about a programming tool that repeats a set of instructions until a specified condition - loops. When we see that a process has to repeat multiple times in a row, we write a loop

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Loops.js
@@ -0,0 +1,4 @@
+
+
+/* I learned about a programming tool that repeats a set of instructions until a
+specified condition - loops. When we see that a process has to repeat multiple times in a row, we write a loop */
