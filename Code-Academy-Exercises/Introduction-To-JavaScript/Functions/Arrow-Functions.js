e4e0ebdb36021e8081737457fc3642ecb14cf5b1
meaella <menaelena@yandex.com>
2019-01-31 18:04:36 -0700

    I learned about the arrow functions that remove the keyword "function", instead, first includes the parameters inside the ( ) and then add an arrow => that points to the function body surrounded in { }

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Arrow-Functions.js
@@ -0,0 +1,11 @@
+
+const plantNeedsWater = day => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/* I learned about the arrow functions that remove the keyword "function",
+instead, first includes the parameters inside the ( ) and then add an arrow => that points to the function body surrounded in { } */
