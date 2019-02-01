9bcfb8755f0f2aca2e87205743ad8c0b69dba6b7
meaella <menaelena@yandex.com>
2019-02-01 13:41:44 -0700

    I learned While most new browser versions support the majority of the ES6 library, there are still a couple sources of compatibility issues: 1. Some users have not updated to the latest, ES6 supported web browser version. 2. A few ES6 features, like modules, are still not supported by most web browsers.

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-1.js
@@ -0,0 +1,6 @@
+
+
+/* I learned While most new browser versions support the majority of the ES6 library, 
+there are still a couple sources of compatibility issues:
+1. Some users have not updated to the latest, ES6 supported web browser version.
+2. A few ES6 features, like modules, are still not supported by most web browsers. */
