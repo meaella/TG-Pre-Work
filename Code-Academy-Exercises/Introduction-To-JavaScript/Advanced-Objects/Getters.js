3971500a4aa9870eacbb5fa0ad9f38d9ca2a1def
meaella <menaelena@yandex.com>
2019-02-01 16:18:31 -0700

    I learned about getters -  methods that get and return the internal properties of an object

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Advanced-Objects/Getters.js
@@ -0,0 +1,17 @@
+
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    } else {
+      return "System malfunction: cannot retrieve energy level"
+    }
+  }
+};
+
+console.log(robot.energyLevel);
+
+// I learned about getters -  methods that get and return the internal properties of an object
