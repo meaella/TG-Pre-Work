bf25ef655359c08558f3c4ba114a7885cb82946b
meaella <menaelena@yandex.com>
2019-02-01 11:43:18 -0700

    I learned that we can use dot notation to access the properties and methods of built-in objects and data instances

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Accessing-Properties.js
@@ -0,0 +1,5 @@
+
+let crewCount = spaceship.numCrew;
+let planetArray = spaceship.flightPath;
+
+// I learned that we can use dot notation to access the properties and methods of built-in objects and data instances
