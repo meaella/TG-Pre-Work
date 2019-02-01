3da60ebdd108eeecaa2e9bebe8ee6e2a8ea48e5c
meaella <menaelena@yandex.com>
2019-02-01 12:05:09 -0700

    I learned about that objects are often nested, an object might have another object as a property

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Objects/Nested-Objects.js
@@ -0,0 +1,5 @@
+
+let capFave = spaceship.crew.captain['favorite foods'][0];
+let firstPassenger = spaceship.passengers[0];
+
+// I learned about that objects are often nested, an object might have another object as a property
