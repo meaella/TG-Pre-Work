dc9b568963bc16b9e09f53bfcd22e1a3b03492a3
meaella <menaelena@yandex.com>
2019-01-31 20:37:55 -0700

    I learned about shift(), unshift(), slice(), tindexOf() methods

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Arrays/More-Array-Methods.js
@@ -0,0 +1,14 @@
+
+groceryList.shift();
+console.log(groceryList);
+
+groceryList.unshift('popcorn');
+console.log(groceryList);
+
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
+
+const pastaIndex = groceryList.indexOf('pasta');
+console.log(pastaIndex);
+
+// I learned about shift(), unshift(), slice(), tindexOf() methods
