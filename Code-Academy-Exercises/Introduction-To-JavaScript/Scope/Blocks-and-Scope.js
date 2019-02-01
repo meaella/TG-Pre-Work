29091a27479625081a6b57a283c7d644aca2b6a4
meaella <menaelena@yandex.com>
2019-01-31 18:22:43 -0700

    I learned about a block inside a set of curly braces {}. Blocks help us group one or more statements together and serve as an important structural marker for our code

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Blocks-and-Scope.js
@@ -0,0 +1,9 @@
+const city = 'New York City';
+function logCitySkyline(){
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city;
+}
+console.log(logCitySkyline());
+
+/* I learned about a block inside a set of curly braces {}.
+Blocks help us group one or more statements together and serve as an important structural marker for our code */
