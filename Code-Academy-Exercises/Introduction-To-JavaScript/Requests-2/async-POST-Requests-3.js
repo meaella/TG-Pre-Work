d32a43f9cacf7c4d30ad33c1ca30ba685432c1b9
meaella <menaelena@yandex.com>
2019-02-01 21:18:05 -0700

    I learned how I to retrieve Rebrandly API key to access the Rebrandly API

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-POST-Requests-3.js
@@ -0,0 +1,24 @@
+
+// AJAX functions
+// Code goes here
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
+			method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+				'apikey': apiKey
+      }
+    });
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+// I learned how I to retrieve Rebrandly API key to access the Rebrandly API.
