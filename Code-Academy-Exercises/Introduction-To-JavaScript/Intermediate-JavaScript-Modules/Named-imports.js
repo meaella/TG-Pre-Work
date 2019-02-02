3c7faf0da781a338689097cc2c3aef6a38e7979b
meaella <menaelena@yandex.com>
2019-02-01 18:55:56 -0700

    I learned about that to import objects stored in a variable, we use the import  keyword and include the variables in a set of {}

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Named-imports.js
@@ -0,0 +1,20 @@
+
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+function displayStaffStatus() {
+  availableAirplanes.forEach(function(element) {
+   console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
+  });
+}
+
+displayStaffStatus();
+/* I learned about that to import objects stored in a variable, we use the import
+ keyword and include the variables in a set of {} */
