3fad90db9467a32925716ddd552f7128123e2ac0
meaella <menaelena@yandex.com>
2019-02-01 20:53:29 -0700

    I learned how use functionality introduced in ES8: async and await

--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-1.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-GET-Requests-1.js
@@ -1,3 +1,14 @@
 
-
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch (error) {
+    console.log(error);
+  }
+}
 // I learned how use functionality introduced in ES8: async and await
