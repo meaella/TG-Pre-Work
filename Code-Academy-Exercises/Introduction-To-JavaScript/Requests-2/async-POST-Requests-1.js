0a6fcb3f6133a3f7af3ed969c117ecc9b09691ea
meaella <menaelena@yandex.com>
2019-02-01 21:08:36 -0700

    I learned how made the async POST request.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-POST-Requests-1.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/async-POST-Requests-1.js
@@ -1,17 +1,2 @@
 
-const getData = async () => {
-  try {
-    const response = await fetch('https://api-to-call.com/endpoint', {
-      method: 'POST',
-      body: JSON.stringify({id: 200})
-    })
-    if(response.ok){
-      const jsonResponse = await response.json();
-      return jsonResponse;
-    }
-    throw new Error('Request failed!');
-  } catch(error) {
-    console.log(error);
-  }
-}
-// I learned how made an async GET request
+// I learned how made the async POST request.
