589bb0913591316fc0ddaf5cf005193bd0a9682d
meaella <menaelena@yandex.com>
2019-02-01 19:28:04 -0700

    I learned how write code for an AJAX GET request using an XMLHttpRequest object

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-1/XHR-GET-Requests-2.js
@@ -0,0 +1,13 @@
+
+const xhr = new XMLHttpRequest();
+const url = "https://api-to-call.com/endpoint";
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+	}
+};
+xhr.open('GET', url);
+xhr.send();
+
+// I learned how write code for an AJAX GET request using an XMLHttpRequest object.
