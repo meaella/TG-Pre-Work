9daa9ca5549f3d1c22b2b6f322ff9a9a9272c075
meaella <menaelena@yandex.com>
2019-02-01 18:42:01 -0700

    I learned that we can get modules by defining a module in one file and making the module available for use in another file

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/module-exports.js
@@ -0,0 +1,7 @@
+
+const Airplane = {};
+Airplane.myAirplane = "StarJet";
+module.exports = Airplane;
+
+/* I learned that we can get modules by defining a module in one file and making
+ the module available for use in another file */
