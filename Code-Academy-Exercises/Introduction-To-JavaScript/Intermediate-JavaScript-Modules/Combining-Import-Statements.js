66be787456ab7359a232328407ecd81f3923e76a
meaella <menaelena@yandex.com>
2019-02-01 19:10:55 -0700

    I learned that we can import the collection of objects and functions with the same data

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Combining-Import-Statements.js
@@ -0,0 +1,7 @@
+
+
+import { availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+import meetsSpeedRangeRequirements from './airplane';
+
+// I learned that we can import the collection of objects and functions with the same data
