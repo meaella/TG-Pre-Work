4edce1fdb8cbe01b8dd79c92736c380a3fd672bd
meaella <menaelena@yandex.com>
2019-02-01 18:49:40 -0700

    I learned that JavaScript has implemented a new more readable and flexible syntax for exporting modules. These are usually broken down into one of two techniques, default export and named exports

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/export-default.js
@@ -0,0 +1,18 @@
+let Airplane = {};
+
+Airplane.availableAirplanes = [
+{
+  name: 'AeroJet',
+  fuelCapacity: 800
+ },
+ {
+  name: 'SkyJet',
+  fuelCapacity: 500
+ }
+];
+
+export default Airplane;
+
+/* I learned that JavaScript has implemented a new more readable and flexible 
+syntax for exporting modules. These are usually broken down into one of two techniques,
+default export and named exports */
