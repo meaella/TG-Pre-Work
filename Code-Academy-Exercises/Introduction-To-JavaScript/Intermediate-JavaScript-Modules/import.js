97387f56c20ecd8730f4ca0b839cad8662bc42cc
meaella <menaelena@yandex.com>
2019-02-01 18:51:13 -0700

    I learned about ES6 module syntax also introduces the import keyword for importing objects

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/import.js
@@ -0,0 +1,11 @@
+import Airplane from './airplane';
+
+function displayFuelCapacity() {
+  Airplane.availableAirplanes.forEach(function(element){
+  console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+displayFuelCapacity();
+
+// I learned about ES6 module syntax also introduces the import keyword for importing objects
