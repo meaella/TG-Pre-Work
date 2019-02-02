41e97d9c918ca43742551b6d0f2aa106f0881b26
meaella <menaelena@yandex.com>
2019-02-01 20:24:49 -0700

    I learned how take the information that was returned with the promise and manipulate the webpage

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-GET-Requests-4.js
@@ -0,0 +1,19 @@
+
+// AJAX function
+const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+
+  fetch(endpoint).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  })
+}
+
+// I learned how take the information that was returned with the promise and manipulate the webpage
