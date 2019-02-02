7e14e39ce0191574bd77c2256094f5f1baa401c4
meaella <menaelena@yandex.com>
2019-02-01 20:49:19 -0700

    I learned how work with another .then() to the chain to finally make the information available

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Requests-2/fetch-POST-Requests-5.js
@@ -0,0 +1,25 @@
+// AJAX functions
+const shortenUrl = () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten})
+
+	fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  })
+}
+
+// I learned how work with another .then() to the chain to finally make the information available
