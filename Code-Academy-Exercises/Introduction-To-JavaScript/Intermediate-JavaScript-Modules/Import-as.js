56b0c1974151365fa540a5c56fc12c296fe77893
meaella <menaelena@yandex.com>
2019-02-01 19:05:30 -0700

    I learned that to import named export aliases with the as keyword, we add the aliased variable in our import statement

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Import-as.js
@@ -0,0 +1,6 @@
+
+import {availableAirplanes, flightRequirements, meetsStaffRequirements,
+meetsSpeedRangeRequirements} from './airplane';
+
+/* I learned that to import named export aliases with the as keyword, we add the
+ aliased variable in our import statement */
