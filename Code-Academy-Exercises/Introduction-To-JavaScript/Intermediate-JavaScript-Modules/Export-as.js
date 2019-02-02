9575759bb4c46cf9acd81a1d95e5a3024fa0a4b7
meaella <menaelena@yandex.com>
2019-02-01 19:03:02 -0700

    I learned that Named exports also conveniently offer a way to change the name of variables when we export or import them. We can do this with the as keyword

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Intermediate-JavaScript-Modules/Export-as.js
@@ -0,0 +1,6 @@
+
+export { availableAirplanes as aircrafts, flightRequirements as flightReqs,
+   meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs };
+   
+/* I learned that Named exports also conveniently offer a way to change the name
+of variables when we export or import them. We can do this with the as keyword */
